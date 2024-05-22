3255ee68562b0f6e2b44295f10e702c73c93905f
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-05-22 12:16:53 +0330

    delete note and completed note created

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,22 @@ const App = () => {
   const addNoteHandler = (newNote) => {
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
+  const deleteNoteHandler = (id) => {
+    // const filteredNotes = notes.filter((note) => note.id !== id);
+    // setNotes(filteredNotes);
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+  };
+  const completeNoteHandler = (e) => {
+    const noteId = Number(e.target.value);
+    // const newNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === noteId ? { ...note, completed: !note.completed } : note
+      )
+    );
+  };
   return (
     <div>
       <div className="flex items-center justify-evenly mb-12 border-b border-b-gray-400 py-2">
@@ -15,7 +31,11 @@ const App = () => {
       <div className="flex flex-col md:flex-row justify-between gap-8">
         <AddNewNote onAddNote={addNoteHandler} />
         <div className="w-full md:w-2/3">
-          <NoteList notes={notes} />
+          <NoteList
+            notes={notes}
+            onDelete={deleteNoteHandler}
+            oncomplete={completeNoteHandler}
+          />
         </div>
       </div>
     </div>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,8 +1,13 @@
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, onDelete, oncomplete }) => {
   return (
     <div>
       {notes.map((note) => (
-        <NoteItem key={note.id} note={note} />
+        <NoteItem
+          key={note.id}
+          note={note}
+          onDelete={onDelete}
+          oncomplete={oncomplete}
+        />
       ))}
     </div>
   );
@@ -10,7 +15,7 @@ const NoteList = ({ notes }) => {
 
 export default NoteList;
 
-const NoteItem = ({ note }) => {
+const NoteItem = ({ note, onDelete, oncomplete }) => {
   const options = {
     year: "numeric",
     month: "long",
@@ -18,17 +23,34 @@ const NoteItem = ({ note }) => {
   };
   return (
     <div className="bg-white p-4 rounded-xl mb-6">
-      <div className="flex items-center justify-between border-b border-b-gray-200 pb-1 mb-3">
+      <div
+        className={`flex items-center justify-between border-b border-b-gray-200 pb-1 mb-3 ${
+          note.completed ? "opacity-80" : ""
+        }`}>
         <div className="flex flex-col">
-          <p className="font-bold text-xl text-slate-800 mb-2">{note.title}</p>
+          <p
+            className={`font-bold text-xl text-slate-800 mb-2 ${
+              note.completed ? "line-through" : ""
+            }`}>
+            {note.title}
+          </p>
           <p className="text-lg text-gray-400 mb-2">{note.description}</p>
         </div>
         <div className="flex items-center gap-4">
-          <button>❌</button>
-          <input type="checkbox" name="" id="" />
+          <button onClick={() => onDelete(note.id)} className="text-sm">
+            ❌
+          </button>
+          <input
+            onChange={oncomplete}
+            value={note.id}
+            className="form-checkbox rounded-sm"
+            type="checkbox"
+            name={note.id}
+            id={note.id}
+          />
         </div>
       </div>
-      <div className="text-gray-500">
+      <div className="text-gray-500 text-left">
         {new Date(note.createdAt).toLocaleDateString("en-US", options)}
       </div>
     </div>
