ef1a5f53360f3d50012b8f90032fd250cfb2e4c0
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-05-23 13:52:35 +0330

    using dynamic component for messaging and final responsive design

--- src/components/NoteStatus.jsx
+++ src/components/NoteStatus.jsx
@@ -1,12 +1,16 @@
+import Message from "./Message";
+
 const NoteStatus = ({ notes }) => {
   const allNotes = notes.length;
   const completedNotes = notes.filter((n) => n.completed).length;
 
   if (!allNotes)
     return (
-      <h2 className="text-sm md:text-xl text-slate-800 font-bold">
-        No Notes has already been added
-      </h2>
+      <Message>
+        <h2 className="text-xl md:text-xl text-slate-800 font-bold">
+        ✅  No Notes has already been added
+        </h2>
+      </Message>
     );
   return (
     <ul className="flex  items-center justify-between gap-x-3 mb-6 text-gray-400 px-4">
